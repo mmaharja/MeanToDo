15376440fb87a6de8a22ae4fe32114ea59d6a33f
M Maharjan <mmaharja@gmail.com>
2015-12-02 17:25:43 -0500

    working mean app. ability to create new to do, and fetch saved to dos. remaining task: ability to delete, polish UI

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,18 @@ var express = require('express');
 var router = express.Router();
 //var mongojs = require("mongojs").connect;
 //var db = mongojs('MeanToDo',['ToDos']);
-var databaseUrl = "meanToDo"; // "username:password@example.com/mydb"
+var databaseUrl = "meanToDo"; 
 var collections = ["toDos"]
-var db = require("mongojs")
-    .connect(databaseUrl, collections);
+var mongojs = require("mongojs")
+ var db =   mongojs(databaseUrl, collections);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/MyToDos', function(req, res){
-    db.ToDos.find(function (err, todos) {
+    db.toDos.find(function (err, todos) {
+        console.log(todos);
         res.json(todos);
     });
     //var todos = [{isCompleted:true, taskDescription:"First Task", dueDate: "11/25/2015"},
@@ -22,8 +23,6 @@ router.get('/MyToDos', function(req, res){
 
 router.post('/MyToDos/AddNewTask', function (req, res) {
     console.log(req.body);
-    db.ToDos.insert(req.body, function (err, docs) {
-        res.json(docs);
-    });
+    db.toDos.insert(req.body);
 })
 module.exports = router;
