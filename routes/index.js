f57001277f233aedbc74e37d5790884d64dcc3e3
M Maharjan <mmaharja@gmail.com>
2015-12-02 19:58:53 -0500

    ability to delete a toDo

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 //var mongojs = require("mongojs").connect;
 //var db = mongojs('MeanToDo',['ToDos']);
-var databaseUrl = "meanToDo"; 
+var databaseUrl = "meanToDo";
 var collections = ["toDos"]
 var mongojs = require("mongojs")
- var db =   mongojs(databaseUrl, collections);
+var db = mongojs(databaseUrl, collections);
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/MyToDos', function(req, res){
+router.get('/myTodos', function (req, res) {
     db.toDos.find(function (err, todos) {
         console.log(todos);
         res.json(todos);
@@ -21,8 +21,14 @@ router.get('/MyToDos', function(req, res){
 
 })
 
-router.post('/MyToDos/AddNewTask', function (req, res) {
+router.post('/myTodos/addNewTodo', function (req, res) {
     console.log(req.body);
     db.toDos.insert(req.body);
-})
+});
+router.delete('/myTodos/deleteTodo/:id', function (req, res) {
+    console.log(req.params._id);
+    db.toDos.remove({_id: db.ObjectId(req.params.id)}, function (err, todo) {
+    });
+});
+
 module.exports = router;
